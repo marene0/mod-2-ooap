735fd31695b6135ac52a8dfd43c48eeafe5a22af
mariana <khortiuk.mariana@gmail.com>
2024-05-14 23:04:27 +0300

    init

---
+++ ooap mod 2/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Originator 
+class ProductionLine
+{
+    public string State { get; set; }
+
+    public ProductionLine(string state)
+    {
+        State = state;
+    }
+
+    public ProductionLineMemento CreateMemento()
+    {
+        return new ProductionLineMemento(State);
+    }
+
+    public void RestoreMemento(ProductionLineMemento memento)
+    {
+        State = memento.State;
+    }
+}
+
+// Memento
+class ProductionLineMemento
+{
+    public string State { get; }
+
+    public ProductionLineMemento(string state)
+    {
+        State = state;
+    }
+}
+
+// Caretaker
+class ProductionLineCaretaker
+{
+    private readonly List<ProductionLineMemento> _mementos = new List<ProductionLineMemento>();
+
+    public void AddMemento(ProductionLineMemento memento)
+    {
+        _mementos.Add(memento);
+    }
+
+    public ProductionLineMemento GetMemento(int index)
+    {
+        return _mementos[index];
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var productionLine = new ProductionLine("Початковий стан");
+        var caretaker = new ProductionLineCaretaker();
+        caretaker.AddMemento(productionLine.CreateMemento());
+        productionLine.State = "Змінений стан";
+        caretaker.AddMemento(productionLine.CreateMemento());
+        productionLine.RestoreMemento(caretaker.GetMemento(0));
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Поточний стан лінії виробництва: " + productionLine.State);
+    }
+}
